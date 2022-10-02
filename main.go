fe59ddfde53ef8a93bbc8165f72fb4e0fbbcf6e8
ashu adhana <ashu.adhana@gmail.com>
2022-10-02 14:25:26 +0530

    building a goquery based selection onHTML function

--- main.go
+++ main.go
@@ -8,29 +8,39 @@ import (
 	colly "github.com/gocolly/colly"
 )
 
-func getinfo(url string) []string {
+type Movie struct {
+	Name     string
+	Uploaded string
+	Magnet   string //link webtorrent
+	Size     string
+	Seeds    string //as of new seeds are already sorted in descending order in 1337x.to
+	Uploader string
+}
 
-	var links []string
-	var magnet string
+func scrapev2(url string) (mvlist []Movie) {
 
-	c := colly.NewCollector(
-		colly.MaxDepth(1),
-	)
-
+	c := colly.NewCollector()
 	// selection criteria
+
+	// scrape the links from the whole page
 	c.OnHTML("a[href]", func(h *colly.HTMLElement) {
 
 		doc := h.DOM
-
 		if attr, exists := doc.Attr("href"); exists {
-			if strings.Contains(attr, "/torrent/") && !strings.HasPrefix(attr, "http://itorrents.org") && !strings.HasPrefix(attr, "http://btcache.me") {
+			if strings.Contains(attr, "/torrent/") && !strings.HasPrefix(attr, "http://") {
 				link := "https://1337x.to" + attr
 				c.Visit(link)
 			}
-			if strings.Contains(attr, "magnet:?") {
-				magnet = attr
-			}
 		}
+
+	})
+
+	c.OnHTML("div.col-9 div.box-info torrent-detail-page  vpn-info-wrap div.box-info-heading clearfix", func(h *colly.HTMLElement) {
+		fmt.Println("this is being hit")
+		metadata := h.DOM
+
+		fmt.Println(metadata.Find("h1").Text())
+
 	})
 
 	// Before making a request print "Visiting ..."
@@ -49,19 +59,11 @@ func getinfo(url string) []string {
 	// tells the collector to start scraping
 	c.Visit(url)
 
-	return links
+	return
 
 }
-
 func main() {
 
-	url := "https://1337x.to/search/spiderman/1/"
-
-	// getinfo(url)
-	links := getinfo(url)
-
-	// print out the results
-	for _, link := range links {
-		fmt.Println(link)
-	}
+	url := "https://1337x.to/torrent/5137042/SpiderMan-No-Way-Home-2021-1080p-HD-TS-V3-Line-Audio-x264-AAC/"
+	scrapev2(url)
 }
